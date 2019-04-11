62f8083bbb33d136869518532a4ed96d9407cd75
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 02:04:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,7 +14,7 @@ end
 
 def third_element(my_third_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_third_element = @taylor_swift.index(2)
+  my_third_element = @taylor_swift(2)
 end
 
 def last_element(my_last_element)
