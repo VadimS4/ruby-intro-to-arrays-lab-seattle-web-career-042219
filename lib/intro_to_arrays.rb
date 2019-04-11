6ff72b2d8f193f7b405e9f3f3231eafd7d93917a
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 02:04:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,7 +34,7 @@ end
 
 def length_of_array(length)
   @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  length = 
+  length = @programming_languages.length
 end
 
 
