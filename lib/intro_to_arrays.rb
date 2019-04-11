1d99db0472c7417f3523961c1f2deafdde38e001
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 01:42:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  new_array = []
+end
+
+def array_with_two_elements
+  array = ["Mercedes", "Audi"]
+end
+
+def my_first_element
+  first_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  first_element.index(0)
+end
+
+def my_third_element
+  third_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  third_element.index(2)
+end
+
+def my_last_element
+  last_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  last_element.index(3)
+end
